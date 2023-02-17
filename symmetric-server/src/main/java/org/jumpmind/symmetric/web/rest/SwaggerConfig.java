37428fa6eb2718783a9cc703294929a0defa7957
billy <shibo_z@126.com>
2023-02-18 01:28:18 +0800

    refactor 将swagger-ui模块合并到server模块中

--- symmetric-server/src/main/java/org/jumpmind/symmetric/web/rest/SwaggerConfig.java
+++ symmetric-server/src/main/java/org/jumpmind/symmetric/web/rest/SwaggerConfig.java
@@ -20,14 +20,10 @@
  */
 package org.jumpmind.symmetric.web.rest;
 
-import java.util.Arrays;
-import java.util.List;
-
 import org.jumpmind.symmetric.Version;
 import org.jumpmind.symmetric.web.WebConstants;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -38,13 +34,11 @@ import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger.web.DocExpansion;
-import springfox.documentation.swagger.web.ModelRendering;
-import springfox.documentation.swagger.web.OperationsSorter;
-import springfox.documentation.swagger.web.TagsSorter;
-import springfox.documentation.swagger.web.UiConfiguration;
-import springfox.documentation.swagger.web.UiConfigurationBuilder;
+import springfox.documentation.swagger.web.*;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
